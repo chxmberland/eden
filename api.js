cea29243e5e93b2fef12be262a0443ccca46e781
Benjamin Chamberland <ben.chamberland@indexexchange.com>
2022-06-27 16:03:25 -0400

    feat: creating basic connection to Atlas cluster

--- api.js
+++ api.js
@@ -1,14 +1,19 @@
-const mongoose = require('mongoose')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const modelLocation = "./backend/database/models/"
-
 const userModel = require(modelLocation + "user-model.js")
+const pass = null
 
-mongoose.connect(
-    "monogdb://localhost/testdb", 
-    () => {
-        console.log("Connected")
-    },
-    e => console.log(e)
-)
+const connectionUri = "mongodb+srv://admin:" + pass + "@atlascluster.wmlg9zu.mongodb.net/?retryWrites=true&w=majority";
+const connectionParams = { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    serverApi: ServerApiVersion.v1 
+}
 
+const client = new MongoClient(connectionUri, connectionParams);
+
+client.connect(err => {
+    // const collection = client.db( [database] ).collection( [collection] );
+    client.close()
+});
