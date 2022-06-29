c51d114966eec501dfd2b0230ea8eb5cf3247888
Benjamin Chamberland <ben.chamberland@indexexchange.com>
2022-06-29 19:52:52 -0400

    task: set-up testing files and module exports to the user API can be tested in external files [EDEN-15]

--- backend/database/api/database-api.test.js
+++ backend/database/api/database-api.test.js
@@ -2,3 +2,5 @@ const databaseApi = require('./database-api.js')
 const userApi = require('./user/user-api.js')
 
 databaseApi.connectToDatabase(databaseApi.connectionUri, databaseApi.connectionParams)
+
+userApi.createUser("0x830830", "benc", "ogj049045h094h5g04h50gh9409g")
